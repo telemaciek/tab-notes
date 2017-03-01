74b889c96651e3c1df1e817ab16ce3425bedb13f
Maciej Szafraniec <szafraniec@gmail.com>
2017-03-01 23:14:58 +0100

    Add title changing based on content

--- src/newtab/note.js
+++ src/newtab/note.js
@@ -24,11 +24,34 @@ function initiateNote() {
 
   function startAutosaving() {
     noteContainer.onkeyup = function(e) {
-      saveNote(noteId, e.target.value)
+      saveNote(noteId, e.target.value);
+      setDocumentTitle(e.target.value);
+    }
+  }
+
+  function setDocumentTitle(content) {
+    var newlineIndex = content.indexOf("\n");
+    switch (true) {
+      case (content === ""):
+      case (content === "\n"):
+      case (content === "\n\n"):
+      case (content === "\n\n\n"):
+        document.title = "UGH";
+        break;
+      case (newlineIndex <= 0):
+        document.title = content.substring(0,20);
+        break;
+      case (newlineIndex <= 20):
+        document.title = content.substring(0,newlineIndex);
+        break;
+      case (newlineIndex > 20):
+        document.title = content.substring(0,20) + "...";
+        break;
     }
   }
 
   showNote(noteId);
+  setDocumentTitle(noteContainer.value);
   startAutosaving(); // Launch autosaving
 }
 window.addEventListener('load', initiateNote);
