838a38a92b754d96b3635a405d039f42941cc0ef
Maciej Szafraniec <szafraniec@gmail.com>
2017-03-03 18:16:46 +0100

    Stop redirecting to new tab after Chrome crashes

--- src/newtab/newtab.js
+++ src/newtab/newtab.js
@@ -4,11 +4,31 @@ function openNoteTab() {
     return date.getTime();
   }
   var nextLink = document.getElementById('nextLink');
-  if (window.history.length === 1) {
-    nextLink.href = "note.html#" + getUniqueId();
-    nextLink.click();
-  } else {
+  if (window.history.length != 1) {
     nextLink.href = 'javaScript:void(0);';
+  } else {
+    nextLink.href = "note.html#" + getUniqueId();
+
+    // Those if's are needed in case Chrome crashes
+    // Without them, extension would prevent user from restoring opened tabs
+    chrome.windows.getAll(function(windows) {
+      // console.log(windows[0].id)
+      if (windows[0].id === 1 && windows.length === 1) { // tab id & window opened
+        chrome.tabs.getAllInWindow(function(window) {
+          if (window.length === 1) { // tabs in window
+            nextLink.href = 'javaScript:void(0);';
+            // console.log('CRASH? So sorry...')
+          } else {
+            // console.log('Let\'s initiate the note...')
+            nextLink.click();
+          }
+        });
+      } else {
+        // console.log('Let\'s initiate the note...')
+        nextLink.click();
+      }
+    });
+
   }
 }
 window.onload = openNoteTab;
