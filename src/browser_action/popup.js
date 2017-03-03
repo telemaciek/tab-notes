2666d44a2a11631bfbd28f661c7d613364c27b90
Maciej Szafraniec <szafraniec@gmail.com>
2017-03-03 19:39:53 +0100

    Export JSON backup into file

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,4 +1,22 @@
-function selectText() {
-  document.getElementById('specials').select();
+function initiatePopup() {
+  function selectText() {
+    document.getElementById('specials').select();
+  }
+  selectText();
+
+  function exportJson() {
+    var exportJsonLink = document.getElementById('exportJsonLink');
+    var notesJsonString = localStorage.getItem("notesStorage");
+    var data = new Blob([notesJsonString], {type: 'text/json'});
+    function downloadExport() {
+      var dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(notesJsonString);
+      var timeOfSave = new Date();
+      exportJsonLink.download = "tab-notes-backup-" + timeOfSave.getTime() + ".json";
+      exportJsonLink.href = dataUri;
+    }
+    exportJsonLink.addEventListener('click', downloadExport);
+  }
+  exportJson();
 }
-window.onload = selectText;
+
+window.onload = initiatePopup;
