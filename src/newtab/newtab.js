9531a438e2ddd1df567cc02097b4ef6e51614be0
Maciej Szafraniec <szafraniec@gmail.com>
2017-03-06 19:14:34 +0100

    Better comments

--- src/newtab/newtab.js
+++ src/newtab/newtab.js
@@ -12,23 +12,18 @@ function openNoteTab() {
     // Those if's are needed in case Chrome crashes
     // Without them, extension would prevent user from restoring opened tabs
     chrome.windows.getAll(function(windows) {
-      // console.log(windows[0].id)
       if (windows[0].id === 1 && windows.length === 1) { // tab id & window opened
         chrome.tabs.getAllInWindow(function(window) {
           if (window.length === 1) { // tabs in window
-            nextLink.href = 'javaScript:void(0);';
-            // console.log('CRASH? So sorry...')
+            nextLink.href = 'javaScript:void(0);'; // don't let user click it
           } else {
-            // console.log('Let\'s initiate the note...')
             nextLink.click();
           }
         });
       } else {
-        // console.log('Let\'s initiate the note...')
         nextLink.click();
       }
     });
-
   }
 }
 window.onload = openNoteTab;
