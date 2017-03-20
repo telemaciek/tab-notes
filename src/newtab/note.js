946ee53da51c2b3f545b5c2f19479b9562607296
Maciej Szafraniec <szafraniec@gmail.com>
2017-03-20 18:20:08 +0100

    Deleted comments and console.logs

--- src/newtab/note.js
+++ src/newtab/note.js
@@ -3,7 +3,6 @@ function initiateNote() {
   var noteId = window.location.hash.substring(1);
 
   function newStorageObj() {
-    // Return initial json (could have example notes)
     var json = {};
     json.version = "1";
     json.notes = {};
@@ -14,44 +13,29 @@ function initiateNote() {
     newStorageObj = newStorageObj();
     var newNotesObj = newStorageObj.notes;
     for (key in oldStorageObj) {
-      // console.log("key");
-      // console.log(key);
-      // console.log(oldStorageObj[key]);
       newNotesObj[key] = {};
       newNotesObj[key].content = oldStorageObj[key];
       newNotesObj[key].dateCreated = Date.now();
       newNotesObj[key].dateModified = Date.now();
-      // console.log(newNotesObj.key);
-      // console.log(newNotesObj.key.content);
     }
-    console.log("newStorageObj");
-    console.log(newStorageObj);
     localStorage.setItem('notesStorage', JSON.stringify(newStorageObj));
     return newStorageObj;
   }
 
   function getNotes() {
-    // get data from localStorage
     var storageString = localStorage.getItem("notesStorage");
-
-    // change string into object
     var storageObj = {};
     if (storageString != null) {
       storageObj = JSON.parse(storageString);
     }
 
     if (!Object.keys(storageObj).length) {
-      console.log("empty object");
       storageObj = newStorageObj();
-    } else {
-      console.log("object exists");
+    }
+    if (storageObj.version === undefined) {
+      storageObj = migrate(storageObj);
     }
 
-    if (storageObj.version === undefined) { // if object isn't empty, but there is no version...
-      console.log("version doesn't exist, migrating");
-      storageObj = migrate(storageObj); // ...migrate old notes into new schema
-    }
-    // return notes object or empty object
     return storageObj.notes || {};
   }
 
@@ -64,33 +48,24 @@ function initiateNote() {
   }
 
   function getNote(noteId) {
-    // get json.notes (getNotes)
     var notesObj = getNotes();
-    // get noteId value
     note = notesObj[noteId] || newNote(noteId);
     return note;
   }
 
   function saveNote(noteId, noteContent) {
-    // get note (getNote)
     var note = getNote(noteId);
-    // change noteContent & date modified
     note.content = noteContent;
     note.dateModified = Date.now();
-    // save note into localStorage
     setNotes(noteId, note);
   }
 
   function setNotes(noteId, note) {
-    // get object from Localstorage,
     var notesObj = getNotes();
-    // change note in notes object
     notesObj[noteId] = note;
-    // construct storage object
     var storageObj = {}
     storageObj.version = "1";
     storageObj.notes = notesObj;
-    // change to string & save into localStorage
     localStorage.setItem('notesStorage', JSON.stringify(storageObj));
   }
 
@@ -139,8 +114,8 @@ function initiateNote() {
     link.href = 'favicon/' + state + '.png';
     document.getElementsByTagName('head')[0].appendChild(link);
   }
+  
   function setProperFavicon(noteTextLength) {
-    // console.log("Character count: " + noteTextLength);
     switch (true) {
       case (noteTextLength === 0):
         changeFavicon('empty');
