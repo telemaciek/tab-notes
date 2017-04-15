29cf89830196eba9d938942e8049c49d1057df33
Maciej Szafraniec <szafraniec@gmail.com>
2017-04-15 20:27:40 +0200

    Listing array instead of object and improved date function

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -9,11 +9,15 @@ function initiatePopup() {
   }
   // selectText();
 
-  var generateDate = function(){
-    var time = new Date();
+  var getDate = function(time){
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    timeOfSave = time.getDate() + " " + months[time.getMonth()] + " " + time.getFullYear() + " at " + time.getHours() + "-" + time.getMinutes();
-    return timeOfSave;
+    var timeNow = new Date();
+    if (timeNow.getDay() === time.getDay() && time.getMonth() === timeNow.getMonth() && time.getFullYear() === timeNow.getFullYear()) {
+      return "Today at " + ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2);
+    } else {
+      absoluteDate = time.getDate() + " " + months[time.getMonth()] + " " + time.getFullYear() + " at " + ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2);
+      return absoluteDate;
+    }
   }
 
   function exportJson() {
@@ -22,7 +26,7 @@ function initiatePopup() {
     var data = new Blob([notesJsonString], {type: 'text/json'});
     function downloadExport() {
       var dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(notesJsonString);
-      var timeOfSave = generateDate();
+      var timeOfSave = getDate(new Date());
       exportJsonLink.download = "tab-notes-backup (" + timeOfSave + ").json";
       exportJsonLink.href = dataUri;
     }
@@ -38,8 +42,12 @@ function initiatePopup() {
     var notesContainer = document.getElementById("notesList");
     var noteElement = "";
 
-    for (key in notesObj) {
-      note = notesObj[key];
+    console.log(notesObj);
+    var notesArray = Object.keys(notesObj).map(function (key) { notesObj[key].id = key; return notesObj[key]; });
+    console.log(notesArray);
+
+    for (var i = 0; i < notesArray.length; i++) {
+      note = notesArray[i];
       var noteSize;
       if (note.content.length === 0) {
         noteSize = "Empty"
@@ -47,19 +55,22 @@ function initiatePopup() {
         noteSize = note.content.length + " characters"
       }
       noteElement += "<div class='note-container'>"
-        + "<a href='/newtab/note.html#" + key + "' target='" + key + "' class='note'>"
+        + "<a href='/newtab/note.html#" + note.id + "' target='" + note.id + "' class='note'>"
         + "<div class='note-content'>"
         + note.content
         + "</div>"
         + "<div class='note-extra'>"
+        + getDate(new Date(note.dateModified))
+        + "<span class='note-size'>("
         + noteSize
+        + ")</span>"
         + "</div>"
         + "</a>"
         + "<div class='note-actions'>"
         + "<div class='note-pin'>"
         + "✨"
         + "</div>"
-        + "<div class='note-delete js-note-delete' data-note-id='" + key + "'>"
+        + "<div class='note-delete js-note-delete' data-note-id='" + note.id + "'>"
         + "<svg width='10px' height='10px' viewBox='0 0 10 10'><g><path d='M5,3.82352941 L1.17647059,1.5720758e-13 L6.61692923e-14,1.17647059 L3.82352941,5 L4.4408921e-13,8.82352941 L1.17647059,10 L5,6.17647059 L8.82352941,10 L10,8.82352941 L6.17647059,5 L10,1.17647059 L8.82352941,1.5720758e-13 L5,3.82352941 Z' fill='#555555'></path></g></svg>"
         + "</div>"
         + "</div>"
